f5fd1e83c0405bc62e22e172bd706454cfee16d4
VicErha Tech <victorchideibere@gmail.com>
2023-10-02 05:22:37 +0100

    adjustment to app

--- login.js
+++ login.js
@@ -1,26 +1,22 @@
 let email = document.getElementById('lemail')
-let password = document.getElementById('lpassword')
+let password = document.getElementById('lpass')
 let msg = document.querySelector('.msg')
 
-
 function loginUser() {
-    // email user enter
     let emailE = email.value
     let passE = password.value
-    // get register user info from LocalStorage
+    let emailLS = JSON.parse(localStorage.getItem('email'))
+    let passLS = JSON.parse(localStorage.getItem('password'))
 
-    let emailLS = localStorage.getItem('email')
-    let passLS = localStorage.getItem('password')
-
-    // validating user input
+    console.log(typeof emailE)
+    console.log(emailE)
+    console.log(typeof emailLS)
+    console.log(emailLS + "LS")
     if (!emailE || !passE) {
         msg.innerHTML = '<p class="alert alert-danger">fill all required field</p>'
     } else {
-
-
-
-        //comparing user info
         emailE === emailLS && passE === passLS ? location.href = 'todo.html' : msg.innerHTML = '<p class="alert alert-warning">username or password not correct</p>'
     }
+
 
 }
